b83cdc9d19daac08fbf6a5b25814636f0b0f6b1e
LuisVogt <luishenriquevogt@gmail.com>
2019-05-24 15:28:06 -0300

    Funções para desenhar retas

--- Step_library_test.ino
+++ Step_library_test.ino
@@ -1,47 +1,47 @@
-#include <Stepper.h> 
+#include <Stepper.h>
 #include <Servo.h>
 
-const int stepsPerRevolution = 500; 
+const int stepsPerRevolution = 500;
 const int passo_min = 1;
 const int passos_para_volta_completa = 2000;
 const int velocidade = 2000;
 //com essa configuração aproximadamente 2000 passos são necessários para uma volta completa
 
-//Inicializa a biblioteca utilizando as portas de 8 a 11 para 
-//ligacao ao motor 
-Stepper myStepper(stepsPerRevolution, A1,A3,A2,A4); 
-Stepper myStepper2(stepsPerRevolution, 9,11,10,12); 
+//Inicializa a biblioteca utilizando as portas de 8 a 11 para
+//ligacao ao motor
+Stepper myStepper(stepsPerRevolution, A1, A3, A2, A4);
+Stepper myStepper2(stepsPerRevolution, 9, 11, 10, 12);
 Servo s;
 int porta_servo = 2;
 int posicao_atual_x = 0;
 int posicao_atual_y = 0;
 
-void setup() 
-{ 
- Serial.begin(9600);
- //Determina a velocidade inicial do motor
- myStepper.setSpeed(60);
- myStepper2.setSpeed(60);
- s.attach(porta_servo);
- s.write(0);
-} 
+void setup()
+{
+  Serial.begin(9600);
+  //Determina a velocidade inicial do motor
+  myStepper.setSpeed(60);
+  myStepper2.setSpeed(60);
+  s.attach(porta_servo);
+  s.write(0);
+}
 
 void lowerServo()
 {
   s.write(180);
   /*for(int i = 0; i < 90; i++)
-  {
+    {
     s.write(i);
-  }*/
+    }*/
 }
 
 void raiseServo()
 {
   s.write(0);
   /*  for(int i = 90; i >= 0; i--)
-  {
+    {
     s.write(i);
-  }*/
+    }*/
 }
 
 void goToPoint(int x, int y)
@@ -51,19 +51,23 @@ void goToPoint(int x, int y)
   stepVetor(vetor_x, vetor_y);
 }
 
-void drawPoligonByRadius(int centerX, int centerY, int radius, int n_sides, float rotate_angle, int sides_to_draw)
+void drawPoligonByRadius(int centerX, int centerY, int radius, int n_sides, float rotate_angle = 0, int sides_to_draw = 9999, bool onBase = true)
 {
   //Essa função desenha os polígonos achando os vértices através de um centro e um raio.
   //O ângulo calculado é o ângulo entre o ponto central e dois vértices adjacentes quaisquer em radianos.
-  float angle = (2*PI)/n_sides;
+  float angle = (2 * PI) / n_sides;
   //3*PI/2 é 270°, e somar metade do ângulo do polinômio garante que um dos lados dele sempre esteja alinhado com o eixo x.
   //Somar um ângulo escolhido pelo usuário permite que ele rotacione o polinômio conforme seja necessário.
-  float start_angle = 3*PI/2 + angle/2 + rotate_angle;
+  float start_angle 
+  if(onBase)  
+    start_angle = (3 * PI / 2) + (angle / 2) + rotate_angle;
+  else
+    start_angle = rotate_angle;
   //Cálculo dos pontos iniciais. Eles são guardados em variáveis diferentes dos pontos subsequentes para permitir que o programa
   //una o último vertice ao primeiro sem se preocupar com erros de ponto flutuantes.
-  int init_x = centerX + cos(start_angle)*radius;
-  int init_y = centerY + sin(start_angle)*radius;
-  
+  int init_x = centerX + cos(start_angle) * radius;
+  int init_y = centerY + sin(start_angle) * radius;
+
   int next_x = 0;
   int next_y = 0;
 
@@ -72,25 +76,54 @@ void drawPoligonByRadius(int centerX, int centerY, int radius, int n_sides, floa
   stepVetor(init_x - posicao_atual_x, init_y - posicao_atual_y);
   lowerServo();
 
-  if(sides_to_draw > n_sides)
+  if (sides_to_draw > n_sides)
     sides_to_draw = n_sides;
 
   for (int i = 1; i <= sides_to_draw ; i++)
   {
-    next_x = centerX + cos(start_angle+angle*i)*radius;
-    next_y = centerY + sin(start_angle+angle*i)*radius;
+    next_x = centerX + cos(start_angle + angle * i) * radius;
+    next_y = centerY + sin(start_angle + angle * i) * radius;
 
     stepVetor(next_x - posicao_atual_x, next_y - posicao_atual_y);
   }
-  if(n_sides == sides_to_draw)
+  if (n_sides == sides_to_draw)
     stepVetor(init_x - posicao_atual_x, init_y - posicao_atual_y);
 }
 
-void drawPoligonBySide(int centerX, int centerY, int side, int n_sides, float rotate_angle = 0, int sides_to_draw = 9999)
+void drawPoligonBySide(int centerX, int centerY, int side, int n_sides, float rotate_angle = 0, int sides_to_draw = 9999, bool onBase = true)
 //Calcula o raio do polígono baseado no tamanho do lado e chama a função de desenhar polígono
 {
-  float angle_center = (2*PI)/n_sides;
-  float angle_edge = angle_center;
+  float angle_center = (2 * PI) / n_sides;
+  int radius = (side/2)/sin(angle_center/2);
+
+  drawPoligonByRadius(centerX,centerY, radius, n_sides, rotate_angle, sides_to_draw, onBase);
+  
+}
+
+void drawLineByCoordinate(int startX, int startY, int endX, int endY)
+{
+  raiseServo();
+
+  stepVetor(startX - posicao_atual_x, startY - posicao_atual_y);
+
+  lowerServo();
+
+  stepVetor(endX - startX, endY - startY);
+}
+
+void drawLineByAngle(int startX, int startY, int distance, int angle)
+{
+  int endX = startX + distance * cos(angle);
+  int endY = startY + distance * sin(angle);
+
+  drawLineByCoordinate(startX, startY, endX, endY);
+}
+
+void drawPoint(int X, int Y)
+{
+  raiseServo();
+  stepVetor(X - posicao_atual_x, Y - posicao_atual_y);
+  lowerServo();
 }
 //void drawCircle(int centerX, int centerY, int radius)
 //{
@@ -106,14 +139,14 @@ void drawPoligonBySide(int centerX, int centerY, int side, int n_sides, float ro
 ////    Serial.print(centerX);
 ////    Serial.print("///");
 ////    Serial.println(posicao_atual_x);
-//    
+//
 ////    Serial.println("sen,center,current:");
 ////    Serial.print(sin(t));
 ////    Serial.print("///");
 ////    Serial.print(centerY);
 ////    Serial.print("///");
 ////    Serial.println(posicao_atual_y);
-//    
+//
 //    stepVetor((centerX + radius*cos(t)-posicao_atual_x), (centerY + radius*sin(t)-posicao_atual_y));
 //    //stepVetor((centerX + radius*cos(t)-posicao_atual_x), 0);
 //  }
@@ -123,7 +156,7 @@ void drawPoligonBySide(int centerX, int centerY, int side, int n_sides, float ro
 
 void testServo()
 {
-  for(int i = 0; i < 180; i++)
+  for (int i = 0; i < 180; i++)
   {
     s.write(i);
     delay(50);
@@ -148,80 +181,97 @@ void stepVetor(int x, int y)
   int negativeY = 1;
   int negativeMain;
   int negativeSecondary;
-  if(x<0){
+  if (x < 0) {
     negativeX = -1;
   }
-  if(y<0){
+  if (y < 0) {
     negativeY = -1;
   }
-  
-  if(x == 0 && y == 0)
+
+  if (x == 0 && y == 0)
   {
     return;
   }
-  else if(x == 0 || y == 0)
+  else if (x == 0 || y == 0)
   {
     zeroSecondary = true;
   }
-  
-  if(abs(x)>abs(y))
+
+  if (abs(x) > abs(y))
   {
     temp_max = abs(x);
-    temp_div = abs(x)/abs(y);
+    temp_div = abs(x) / abs(y);
 
     main_stepper = &myStepper;
     secondary_stepper = &myStepper2;
-    
+
     negativeMain = negativeX;
     negativeSecondary = negativeY;
   }
   else
   {
     temp_max = abs(y);
-    temp_div = abs(y)/abs(x);
-    
+    temp_div = abs(y) / abs(x);
+
     secondary_stepper = &myStepper;
     main_stepper = &myStepper2;
-    
+
     negativeSecondary = negativeX;
     negativeMain = negativeY;
 
   }
 
-  
-  
-  for(int i = 0; i<temp_max; i++)
+
+
+  for (int i = 0; i < temp_max; i++)
   {
-    main_stepper->step(passo_min*negativeMain);
-    if(zeroSecondary)
+    main_stepper->step(passo_min * negativeMain);
+    if (zeroSecondary)
     {
       continue;
     }
-    if(i%temp_div==0)
+    if (i % temp_div == 0)
     {
-      secondary_stepper->step(passo_min*negativeSecondary);
+      secondary_stepper->step(passo_min * negativeSecondary);
     }
   }
 }
 
-bool teste = false;
+void serialEvent()
+{
+  byte bufferSerial[Serial.available];
+  int index = 0;
+  int bufferSize = Serial.readBytes(bufferSerial, Serial.available());
 
-void loop() 
-{ 
- Serial.print("Teste");
-
-  
-  drawPoligonByRadius(1000, 1000, passos_para_volta_completa, 4,0,2);
-  /*for(int i = 3; i < 10; i++)
+  if(index == 0)
   {
+    if(bufferSerial[index] == 0)
+    {
+      Serial.println("Buffer");
+    }
+  }
+  i++;
+  if(index >= bufferSize)
+    return;
+}
+
+void loop()
+{
+  Serial.print("Teste");
+
+
+  drawPoligonByRadius(1000, 1000, passos_para_volta_completa, 4, 0, 2);
+  /*for(int i = 3; i < 10; i++)
+    {
     drawPoligon(0,0,500,i);
     delay(500);
-  }*/
- //drawCircle(0,0,1000);  
- //myStepper.step(passos_para_volta_completa);
- //stepVetor(passos_para_volta_completa,0);
- //stepVetor(0,passos_para_volta_completa);
- //stepVetor(-passos_para_volta_completa,0);
- //stepVetor(0,-passos_para_volta_completa);
- delay(1000);
+    }*/
+  //drawCircle(0,0,1000);
+  //myStepper.step(passos_para_volta_completa);
+  //stepVetor(passos_para_volta_completa,0);
+  //stepVetor(0,passos_para_volta_completa);
+  //stepVetor(-passos_para_volta_completa,0);
+  //stepVetor(0,-passos_para_volta_completa);
+  Serial.write(byte(58));
+  delay(1000);
 }
